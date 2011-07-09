1f8723fd66cdd52d1b2ddc8f4400a35098aa82e9
railsaholic <manoj.mk27@gmail.com>
2011-07-10 01:32:33 +0530

    more layout fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def show_flash
     [:notice, :error, :warning, :done].collect do |key|
-      content_tag(:div, flash[key], :class => "msg #{flash[key]}") unless flash[key].blank?
+      content_tag(:div, flash[key], :class => "msg #{key}") unless flash[key].blank?
     end.join
   end
 end
